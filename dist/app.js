20d6578861cc302aa9836c6f155f33787057ad38
Deivid Veloso <veloso.deivid@loft.com.br>
2018-10-31 09:25:18 -0300

    add graphql and first query

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,15 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var schema_1 = require("./graphql/schema");
 var App = /** @class */ (function () {
     function App() {
         this.express = express();
         this.middleware();
     }
     App.prototype.middleware = function () {
-        this.express.use("/hello", function (req, res, next) {
-            res.send({
-                hello: "Hello World"
-            });
-        });
+        this.express.use("/graphql", graphqlHTTP({ schema: schema_1["default"], graphiql: process.env.NODE_ENV === "development" }));
     };
     return App;
 }());
--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,9 @@
 exports.__esModule = true;
 var http = require("http");
 var app_1 = require("./app");
+var handlersServer_1 = require("./utils/handlersServer");
 var server = http.createServer(app_1["default"]);
-server.listen(3000);
-server.on("listening", function () { return console.log("Listening on port 3000"); });
+var port = handlersServer_1.normalizePort(process.env.PORT || 3000);
+server.listen(port);
+server.on("error", handlersServer_1.onError(server));
+server.on("listening", handlersServer_1.onListening(server));
