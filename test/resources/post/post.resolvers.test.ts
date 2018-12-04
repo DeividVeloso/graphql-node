2a476a216c22d99ec5bd5239e7e7c5da581006c0
Deivid Veloso <veloso.deivid@gmail.com>
2018-12-04 08:46:12 -0200

    add post test

--- test/resources/post/post.resolvers.test.ts
+++ test/resources/post/post.resolvers.test.ts
@@ -4,6 +4,7 @@ import { UserInstance } from "../../../src/models/UserModel";
 
 import * as jwt from "jsonwebtoken";
 import { PostInstance } from "../../../src/models/PostModel";
+import { request } from "http";
 
 describe("Post", () => {
   let userId: number;
@@ -119,6 +120,145 @@ describe("Post", () => {
               expect(post).to.have.keys(["title", "author", "comments"]);
               expect(post.title).to.be.equal("First post");
               expect(post.author.name).to.be.equal("Champs");
+            })
+            .catch(error => handleError(error));
+        });
+        it("should return error with post not found", () => {
+          let body = {
+            query: `
+                      query getPost($id: ID!) {
+                        post(id: $id) {
+                            title
+                          }
+                      }
+                    `,
+            variables: {
+              id: -1
+            }
+          };
+          return chai
+            .request(app)
+            .post("/graphql")
+            .set("content-type", "application/json")
+            .send(JSON.stringify(body))
+            .then(res => {
+              expect(res.body.data.post).to.be.null;
+              expect(res.body.errors).to.be.an("array");
+              expect(res.body).to.have.keys(["data", "errors"]);
+              expect(res.body.errors[0].message).to.equal(
+                "Error: Post id -1 not found"
+              );
+            })
+            .catch(error => handleError(error));
+        });
+      });
+    });
+  });
+  describe("Mutations", () => {
+    describe("application/json", () => {
+      describe("createPost", () => {
+        it("shoulde create a new Post", () => {
+          let body = {
+            query: `
+            mutation createPost($input: PostInput!) {
+              createPost(input: $input) {
+                  id
+                  title
+                  content
+                  author {
+                    name
+                  }
+                }
+            }
+            `,
+            variables: {
+              input: {
+                title: "Novo Post do Champs",
+                content: "Show essa graphql",
+                photo: "http://photo.com"
+              }
+            }
+          };
+
+          return chai
+            .request(app)
+            .post("/graphql")
+            .set("content-type", "application/json")
+            .set("authorization", `Bearer ${token}`)
+            .send(JSON.stringify(body))
+            .then(res => {
+              const createPost = res.body.data.createPost;
+              expect(createPost).to.be.an("object");
+              expect(createPost.title).to.equal("Novo Post do Champs");
+              expect(createPost.content).to.equal("Show essa graphql");
+              expect(parseInt(createPost.id)).to.be.a("number");
+              expect(createPost.author.name).to.equal("Champs");
+            });
+        });
+      });
+      describe("updatePost", () => {
+        it("should update a Post", () => {
+          let body = {
+            query: `
+            mutation updatePost($id: ID!, $input: PostInput!) {
+              updatePost(id: $id, input: $input) {
+                  id
+                  title
+                  content
+                  author {
+                    name
+                  }
+                }
+            }
+            `,
+            variables: {
+              id: postId,
+              input: {
+                title: "Novo Post do Champs",
+                content: "Show essa graphql",
+                photo: "http://photo.com"
+              }
+            }
+          };
+
+          return chai
+            .request(app)
+            .post("/graphql")
+            .set("content-type", "application/json")
+            .set("authorization", `Bearer ${token}`)
+            .send(JSON.stringify(body))
+            .then(res => {
+              const updatePost = res.body.data.updatePost;
+              expect(updatePost).to.be.an("object");
+              expect(updatePost.title).to.equal("Novo Post do Champs");
+              expect(updatePost.content).to.equal("Show essa graphql");
+              expect(parseInt(updatePost.id)).to.be.a("number");
+              expect(updatePost.author.name).to.equal("Champs");
+            });
+        });
+      });
+      describe("deletePost", () => {
+        it("should delete a Post", () => {
+          let body = {
+            query: `
+            mutation deletePost($id: ID!) {
+              deletePost(id: $id)
+            }
+            `,
+            variables: {
+              id: postId
+            }
+          };
+
+          return chai
+            .request(app)
+            .post("/graphql")
+            .set("content-type", "application/json")
+            .set("authorization", `Bearer ${token}`)
+            .send(JSON.stringify(body))
+            .then(res => {
+              const deletePost = res.body.data.deletePost;
+              expect(deletePost).to.be.true;
             })
             .catch(error => handleError(error));
         });
